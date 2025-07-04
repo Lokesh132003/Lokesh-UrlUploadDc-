0d4082bd527f828d3b231a431059bb1b21b10b00
Lokesh132003 <lokeshpentapati6798@gmail.com>
2025-07-04 16:33:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ class Config(object):
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
     # set timeout for subprocess
-    PROCESS_MAX_TIMEOUT = 3600
+    PROCESS_MAX_TIMEOUT = 3
     # watermark file
     DEF_WATER_MARK_FILE = ""
     # see what user do with your bot
